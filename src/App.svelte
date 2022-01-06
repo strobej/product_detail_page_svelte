2be53b9687ca12c8e80ab812ec0d283616739a20
Selma Trobej <selma.trobej@nelly.com>
2022-01-06 14:21:49 +0100

    added data from json

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,22 @@
 <script>
 	export let name;
+	import Image from "./lib/components/Image.svelte";
+	import data from "./lib/data/product-data.json";
 </script>
 
-<main>
+<article>
+	<section>
+
+	</section>
+	<aside>
+
+	</aside>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+</article>
 
 <style>
-	main {
+	article {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -20,10 +28,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+		font-family: 'Montserrat', sans-serif;
 	}
 
 	@media (min-width: 640px) {
-		main {
+		article {
 			max-width: none;
 		}
 	}
